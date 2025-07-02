ffa95b4f97ef79488e3b799af6132243547166b3
Qasim <mianqasim065@gmail.com>
2025-07-02 23:01:48 +0500

    Create FriendList component and Friend Componenent to show friends in a list

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,52 @@ const initialFriends = [
 ];
 
 function App() {
+  const [friends]=useState(initialFriends);
+  
+   return (
+    <div className='app'>
+       <div className='sidebar'>
+          <FriendList friends={friends} />
+       </div>
+    </div>
+  )
 
 }
 function Button({children, onClick}){
   return(
     <button onClick={onClick} className='button' >{children}</button>
+  )
+}
+function FriendList({friends}){
+    return(
+      <ul>
+      {
+          friends.map((frnd)=>(
+          <Friend
+          key={frnd.id}
+          id={frnd.id}
+          imgSrc={frnd.image}
+          name={frnd.name}
+          balance={frnd.balance}
+          
+          />
+          ))
+      }
+      </ul>
+    )
+}
+function Friend({ imgSrc, name, balance}){
+  return(
+      <li >
+           <img src={`${imgSrc}`} alt={`${name}`} />
+          <h3>{`${name}`}</h3>
+          { balance<0 && <p className="red"> You owe {name} ${Math.abs(balance)} </p>}
+          { balance>0 && <p className="green"> {name} owe You ${Math.abs(balance)} </p>}
+          { balance===0 && <p > You and {name} are even  </p>}
+
+          <Button >Select</Button>
+
+      </li>
   )
 }
 export default App
